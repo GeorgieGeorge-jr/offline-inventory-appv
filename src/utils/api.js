551dce017741b0c0f43326d93e0938b0ad26ff28
George George <156202950+GeorgieGeorge-jr@users.noreply.github.com>
2026-03-29 10:22:13 +0100

    baseUrl = "https://offline-inventory-appv-production.up.railway.app";

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,4 +1,3 @@
-export const API_BASE_URL =
-  process.env.EXPO_PUBLIC_API_URL || "http://192.168.21.153:5001";
+export const BASE_URL = "https://offline-inventory-appv-production.up.railway.app";
 // Example:
 // export const API_BASE_URL = "http://192.168.1.5:5001";
