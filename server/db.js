a655ff1847a42b924e1161886f2614c621ad4c8e
George George <156202950+GeorgieGeorge-jr@users.noreply.github.com>
2026-03-29 09:35:42 +0100

    ..

--- server/db.js
+++ server/db.js
@@ -1,21 +1,19 @@
 const mysql = require("mysql2");
 require("dotenv").config();
 
-let dbConfig;
+let db;
 
 if (process.env.MYSQL_URL) {
-  dbConfig = process.env.MYSQL_URL;
+  db = mysql.createConnection(process.env.MYSQL_URL);
 } else {
-  dbConfig = {
+  db = mysql.createConnection({
     host: process.env.DB_HOST || "localhost",
     user: process.env.DB_USER || "root",
     password: process.env.DB_PASSWORD || "",
     database: process.env.DB_NAME || "rims_live",
     port: Number(process.env.DB_PORT || 3306),
-  };
+  });
 }
-
-const db = mysql.createConnection(dbConfig);
 
 db.connect((err) => {
   if (err) {
